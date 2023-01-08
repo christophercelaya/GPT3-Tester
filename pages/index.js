118bf12a71c1b08b07e8abfbedd0da90aa8ce697
Chris Celaya <mr.christophercelaya@gmail.com>
2023-01-08 12:18:48 -0700

    add loading indicator

--- pages/index.js
+++ pages/index.js
@@ -1,122 +1,115 @@
-  import { useState } from 'react'
-  import Head from 'next/head';
-  import Image from 'next/image';
-  import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
-  const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const [isGenerating, setIsGenerating] = useState(false)
-  const [apiOutput, setApiOutput] = useState('')
+const Home = () => {
+	const [userInput, setUserInput] = useState("");
+	const [apiOutput, setApiOutput] = useState("");
+	const [isGenerating, setIsGenerating] = useState(false);
 
-  const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
-    
-    console.log("Calling OpenAI...")
-    const response = await fetch('/api/generate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userInput }),
-    });
-  
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text)
-  
-    setApiOutput(`${output.text}`);
-    setIsGenerating(false);
-  }  
-  
-  const onUserChangedText = (event) => {console.log(event.target.value);
-    setUserInput(event.target.value);
-  };
+	const callGenerateEndpoint = async () => {
+		setIsGenerating(true);
 
+		console.log("Calling OpenAI...");
+		const response = await fetch("/api/generate", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ userInput }),
+		});
 
+		const data = await response.json();
+		const { output } = data;
+		console.log("OpenAI replied...", output.text);
 
+		setApiOutput(`${output.text}`);
+		setIsGenerating(false);
+	};
 
-  return (
-  <div className="root">
-    <div className="container">
-      <div className="header">
-        <div className="header-title">
-          <h1>AI Song Writer</h1>
-        </div>
-        <div className="header-subtitle">
-          <h2>Have a song written by ai.</h2>
-        </div>
-      </div>
+	const onUserChangedText = (event) => {
+		console.log(event.target.value);
+		setUserInput(event.target.value);
+	};
 
-  {/* Text Area*/}
-  <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText}
-  />
-  {/* New Code */}
-  <div className="prompt-buttons">
-    <a className="generate-button" onClick={callGenerateEndpoint}>
-      <div className="generate">
-        <p>Generate</p>
-      </div>
-    </a>
-  </div>
+	return (
+		<div className="root">
+			<Head>
+				<title>GPT-3 Assistant | chriscelaya</title>
+			</Head>
+			<div className="container">
+				<div className="header">
+					<div className="header-title">
+						{/* Change your headline here */}
+						<h1>AI Super Assistant</h1>
+					</div>
+					<div className="header-subtitle">
+						<h1>Examples:</h1>
+            <h2>
+							Find me a chinese restraunt downtown in Houston, Texas.
+						</h2>
+            <h2>
+              Write an essay about Nikola Tesla and his contributions to technology.
+            </h2>
+            <h2>
+              Create a blog post about cats and ice cream in the style of Joe Rogan.
+            </h2>
+					</div>
+				</div>
 
+				<div className="prompt-container">
+					<textarea
+						placeholder="Create a blog post about cats and ice cream in the style of Joe Rogan."
+						className="prompt-box"
+						value={userInput}
+						onChange={onUserChangedText}
+					/>
+					<div className="prompt-buttons">
+						<a
+							className={
+								isGenerating ? "generate-button loading" : "generate-button"
+							}
+							onClick={callGenerateEndpoint}
+						>
+							<div className="generate">
+								{isGenerating ? (
+									<span className="loader"></span>
+								) : (
+									<p>Generate</p>
+								)}
+							</div>
+						</a>
+					</div>
+					{/* New code I added here */}
+					{apiOutput && (
+						<div className="output">
+							<div className="output-header-container">
+								<div className="output-header">
+									<h3>Output</h3>
+								</div>
+							</div>
+							<div className="output-content">
+								<p>{apiOutput}</p>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+			<div className="badge-container grow">
+				<a
+					href="https://christophercelaya.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="badge">
+						<Image src={buildspaceLogo} alt="buildspace logo" />
+						<p>built by chriscelaya</p>
+					</div>
+				</a>
+			</div>
+		</div>
+	);
+};
 
-  
-  
-  <div className="prompt-container">
-  <div className="prompt-buttons">
-
-  {/* Generate Button */}
-  <a className="generate-button" onClick={callGenerateEndpoint}>
-    <div className="generate">
-      <p>Generate</p>
-    </div>
-  </a>
-
-  {/* Button loading animation */ }
-  <div className="prompt-buttons">
-  <a
-  className={isGenerating ? 'generate-button loading' : 'generate-button'}
-  onClick={callGenerateEndpoint}
-  >
-  <div className="generate">
-  {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-  </div>
-  </a>
-  </div>
-
-
-  {/* API OUTPUT */}
-  <div className="output">
-  <div className="output-header-container">
-    <div className="output-header">
-      <h3>Output</h3>
-    </div>
-  </div>
-  <div className="output-content">
-    <p>{apiOutput}</p>
-  </div>
-  </div>
-  )
-
-
-{/* Buildspace Logo */}
-  </div>
-      </div>
-    </div>
-    <div className="badge-container grow">
-      <a
-        href="https://christophercelaya.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="badge">
-          <Image src={buildspaceLogo} alt="buildspace logo" />
-          <p>by chriscelaya</p>
-        </div>
-      </a>
-    </div>
-  </div>
-  );
-  };
-
-  export default Home;
+export default Home;
