0a915e41f92eca121f4591f1c2a80f38bc22a112
Chris Celaya <im-mr-chris@protonmail.ch>
2023-01-08 22:56:30 -0700

    Update generate.js
    Updated to screenwriter prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,7 +6,7 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "In the style of Notorious BIG, ";
+const basePromptPrefix = "I want you to act as a screenwriter. You will develop an engaging and creative script for either a feature length film, or a Web Series that can captivate its viewers. Start with coming up with interesting characters, the setting of the story, dialogues between the characters etc. Once your character development is complete - create an exciting storyline filled with twists and turns that keeps the viewers in suspense until the end. My first request is";
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
